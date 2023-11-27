7612120c819702c3fb44f6da56155ed99105db5a
meharasbeham <128901764+meharasbeham@users.noreply.github.com>
2023-11-27 11:57:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,99 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <p className="par">
+      <input Name="name" placeholder="search"></input>    
+      </p>
+      <h3>Sales information</h3>
+ 
+  <div className="col">
+   <div>Customer</div> 
+   <div>Invoice ID</div>
+   <div>Start Date</div>
+   <div>End Date</div>
+   </div>  
+   <div className="input">
+   <input name="myinput"placeholder="Enter customer name"></input>
+   <input name="myinput"placeholder="Enter invoice id"></input>
+   <input name="myinput"placeholder="Start date"></input>
+   <input name="myinput"placeholder="End date"></input>
+   </div>
+  
+    <div className="col-1">
+      <div className="d"></div>
+       <div> Invoice Id</div>
+       <div>Date</div> 
+        <div>Customer</div>
+       <div>Payable Amount</div> 
+       <div>Paid Amount</div>
+       <div>Due</div>
+       </div>
+       <div className="col-3">
+       <div className="d"></div>
+       <div id="no-1"> #Invoice Id</div>
+       <div>Date</div> 
+        <div>Customer</div>
+       <div>PayableAmt</div> 
+       <div>PaidAmt</div>
+       <div>Due</div>
+       </div>
+       <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC1</div>
+       <div>23/11/2023</div> 
+        <div>ANPU</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+         </div>
+    <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC2</div>
+       <div>23/11/2023</div> 
+        <div>BALA</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+         </div>
+    <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC3</div>
+       <div>23/11/2023</div> 
+        <div>CHAR</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+         </div>
+    <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC4</div>
+       <div>23/11/2023</div> 
+        <div>DEEP</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+           </div>
+    <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC5</div>
+       <div>23/11/2023</div> 
+        <div>ELAN</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+          </div>
+    <div className="col-2">
+       <div className="d"></div>
+       <div id="no-1"># ABC6</div>
+       <div>23/11/2023</div> 
+        <div>FATHI</div>
+       <div>2000</div> 
+       <div className="space">000</div>
+       <div className="space">000</div>
+           </div>
+</div>
+
+           )};
+   export default App;
